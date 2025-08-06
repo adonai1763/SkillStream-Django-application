f5238d4a7b6e065ff1650a733a9fe66d19141143
adonai <adonaiteklit24@gmail.com>
2025-08-06 18:43:42 +0200

    SIMPLE: Just migrate and start gunicorn - no more complexity

--- start.sh
+++ start.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-set -e
-
-echo "=== SKILLSTREAM STARTUP ==="
-echo "PORT: $PORT"
-echo "DJANGO_SETTINGS_MODULE: $DJANGO_SETTINGS_MODULE"
-
-echo "Running migrations..."
-python manage.py migrate --noinput
-
-echo "Starting gunicorn on port $PORT..."
-exec gunicorn config.wsgi:application \
-    --bind 0.0.0.0:$PORT \
-    --workers 1 \
-    --timeout 60 \
-    --access-logfile - \
-    --error-logfile - \
-    --log-level debug
+python manage.py migrate
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
